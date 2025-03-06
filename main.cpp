17c0e30a585a807e3a20cd7b2ac8d223919a8d95
Link2war <link2war.riders@gmail.com>
2025-03-06 17:35:50 +0100

    map avec sprite

--- main.cpp
+++ main.cpp
@@ -24,60 +24,30 @@
 
 #include <BBOP/Graphics.h>
 
-
 using namespace std;
 
-float fast_rand(float min, float max) {
-    static unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    seed = (214013 * seed + 2531011); // LCG constants
-    return min + (seed >> 16) * (1.0f / 65535.0f) * (max - min); // Scale to [min, max]
-}
+
 
 int main() {
   
   GLFWwindow * window;
   bbopInit(1920,1080,"name",window);
+
+  Scene scene;
+
+  Map map;
+  Camera cam;
   
-  Scene scene(1.0f, Vector3i(255,255,255));
-  cerr<<"l42"<<endl;
 
-   // Masquer le curseur de la souris
-  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
-  cerr<<"l45"<<endl;
-
-  // fond 
-  RectangleShape rectangle;
-  cerr<<"l50"<<endl;
-
-  Perso p;
-  cerr<<"l53"<<endl;
 
   while (!glfwWindowShouldClose(window))
   {
     // Nettoyage de la fenêtre
     bbopCleanWindow(window, Vector3i(0,0,0),1.0);
-    cerr<<"l55"<<endl;
-
-    // On 'active' la scene pour donner au shader opengl les variables uniforms
+  
     scene.Use();
-    cerr<<"l59"<<endl;
+    map.Draw(scene, cam);
 
-    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS){
-      p.stateUpdate(0);
-    }
-    if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS){
-      p.stateUpdate(1);
-    }
-    if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
-      p.stateUpdate(2);
-    }
-    cerr<<"l70"<<endl;
-
-    p.Update();
-    cerr<<"l73"<<endl;
-
-    scene.Draw(p);
-    cerr<<"l76"<<endl;
 
     // Faire le rendue du frame buffer de la fenêtre
     scene.render();
